5500dc23b3af6c174a6ae33b249456d4dc61c753
Ilie Chicioroaga <106095042+XxILUSHAxX@users.noreply.github.com>
2024-05-16 20:22:32 +0200

    add vue Project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <HomePage msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import HomePage from "@/components/HomePage.vue";
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+    HelloWorld,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 100px;
+}
+html, body {
+  background-color: #dcdcdc; /* Helles Grau */
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <!-- Hier kommen deine anderen Elemente hin -->
+    <!-- <h1>TaskMaster</h1> -->
+    <img alt="Task Master Logo" src="../assets/TaskMasterLogo.png" style="width: 400px; height: 400px;">
+    <p>"Spaßig und struktuiert durch den Arbeitstag" </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
